4e3ab01f629a822a0fa2e52856a18b1ef295dbd2
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for bc80a7c5-3193-48a5-a69b-e5d5a5e4392b-bug-4

--- internal/persistence/validate.go
+++ internal/persistence/validate.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"scenicpermit/internal/application"
 	"scenicpermit/internal/audit"
 	"scenicpermit/internal/domain"
 )
@@ -69,6 +70,64 @@ func validateDatabase(db database) error {
 	chain := audit.VerifyChain(db.Permits)
 	if !chain.Valid {
 		return fmt.Errorf("凭据摘要链损坏：%s", chain.Message)
+	}
+	if err := validateIdempotency(db); err != nil {
+		return err
+	}
+	return nil
+}
+
+// validateIdempotency 逐条校验幂等缓存中的响应与所属投影可达历史的一致性，
+// 防止跨批次、修订号越界或状态不匹配但 JSON 合法的响应被重放流程当作成功重放。
+func validateIdempotency(db database) error {
+	for batchID, responses := range db.Idempotency {
+		if len(responses) == 0 {
+			continue
+		}
+		rawAggregate, exists := db.Batches[batchID]
+		if !exists {
+			return fmt.Errorf("幂等缓存引用未知批次 %s", batchID)
+		}
+		var aggregate domain.Aggregate
+		if err := json.Unmarshal(rawAggregate, &aggregate); err != nil {
+			return fmt.Errorf("批次 %s 投影损坏: %w", batchID, err)
+		}
+		approvedRevision := int64(0)
+		for _, event := range db.Events[batchID] {
+			if event.Action == "batch.approved" {
+				approvedRevision = event.Revision
+			}
+		}
+		for key, raw := range responses {
+			var result application.CommandResult
+			if err := json.Unmarshal(raw, &result); err != nil {
+				return fmt.Errorf("批次 %s 幂等键 %s 响应损坏: %w", batchID, key, err)
+			}
+			if result.BatchID != "" && result.BatchID != batchID {
+				return fmt.Errorf("批次 %s 幂等键 %s 引用其他批次 %s", batchID, key, result.BatchID)
+			}
+			if result.Revision > 0 && result.Revision > aggregate.Batch.Revision {
+				return fmt.Errorf("批次 %s 幂等键 %s 修订号 %d 超出可达历史 %d", batchID, key, result.Revision, aggregate.Batch.Revision)
+			}
+			if result.State == "" {
+				continue
+			}
+			switch domain.BatchState(result.State) {
+			case domain.BatchDraft, domain.BatchSubmitted, domain.BatchInspecting, domain.BatchReady, domain.BatchApproved:
+			default:
+				return fmt.Errorf("批次 %s 幂等键 %s 状态 %q 无效", batchID, key, result.State)
+			}
+			if domain.BatchState(result.State) == domain.BatchApproved {
+				if approvedRevision == 0 || result.Revision != approvedRevision ||
+					aggregate.Batch.State != domain.BatchApproved || aggregate.Permit == nil {
+					return fmt.Errorf("批次 %s 幂等键 %s 声明已批准但与可达历史不一致", batchID, key)
+				}
+				continue
+			}
+			if result.Revision == aggregate.Batch.Revision && domain.BatchState(result.State) != aggregate.Batch.State {
+				return fmt.Errorf("批次 %s 幂等键 %s 状态 %q 与当前投影 %q 不一致", batchID, key, result.State, aggregate.Batch.State)
+			}
+		}
 	}
 	return nil
 }
